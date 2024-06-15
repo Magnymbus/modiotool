042cfcd98f0d62be46bcbea722c4b9c620d67a50
Magnymbus <tybuddy11@gmail.com>
2024-06-15 05:31:48 -0600

    Merged superfluous functions, simplified flow, and made more "pythonic"

--- syncer.py
+++ syncer.py
@@ -10,70 +10,42 @@ CACHE_PATH = './cache/'
 MODS_PATH = './mods/'
 
 
-def create_new_config():
-    with open(CONFIG_PATH, 'w') as config_file:
-        config_file.write(
-            json.dumps(
-                {
-                    "api_key": 'api_key',
-                    "oauth2": 'oauth2',
-                    "game": 'game',
-                },
-                indent=4
-            )
-        )
+def create_default_config():
+    default_config = {"api_key": 'api_key', "oauth2": 'oauth2', "game": 'game'}
+    update_config(default_config)
 
 
-def get_new_client():
-    api_key = input('Please enter your API key.\n>: ')
-    client = modio.Client(api_key=api_key)
-    client.email_request(input('Please enter Mod.IO Email Address. (A security code will be emailed for authentication)\n>: '))
-    oauth2 = client.email_exchange(input('Please enter the emailed security code. \n>: '))
-    return api_key, oauth2, client
-
-
-def read_config_file():
+def load_config():
     with open(CONFIG_PATH, 'r') as config_file:
         return json.loads(config_file.read())
 
 
-def write_config_file(config_dict):
+def update_config(config_dict):
     with open(CONFIG_PATH, 'w') as config_file:
         config_file.write(json.dumps(config_dict, indent=4))
 
 
 def get_or_create_client_config():
     if not os.path.exists(CONFIG_PATH):
-        create_new_config()
-    config_dict = read_config_file()
-
+        create_default_config()
+    config_dict = load_config()
     if missing_keys := {"api_key", "oauth2", "game"} - config_dict.keys():
         if {"api_key", "oauth2"} & missing_keys:
-            api_key, oauth2, client = get_new_client()
+            api_key = input('Please enter your API key.\n>: ')
+            client = modio.Client(api_key=api_key)
+            client.email_request(input('Please enter Mod.IO Email Address. (A security code will be emailed for authentication)\n>: '))
+            oauth2 = client.email_exchange(input('Please enter the emailed security code. \n>: '))
             config_dict.update({"api_key": api_key, "oauth2": oauth2})
         if "game" in missing_keys:
             config_dict["game"] = input('Please enter the name of desired Mod.IO game.\n>: ')
-        write_config_file(config_dict)
-
-    if 'client' not in locals():
-        client = modio.Client(api_key=config_dict["api_key"], access_token=config_dict["oauth2"])
+        update_config(config_dict)
+    client = modio.Client(api_key=config_dict["api_key"], access_token=config_dict["oauth2"])
     return config_dict, client
 
 
-def get_subbed_mods():
-    config, client = get_or_create_client_config()
-    gameid = client.get_games(filters=modio.Filter({'name': config["game"]})).results[0].id
-    return client.get_my_subs(filters=modio.Filter({'game_id': gameid})).results
-
-
 def get_highest_file_url(mod):
-    files = mod.get_files().results
-    highest_file = max(files, key=lambda file: parse(file.version or "0.0.0"), default=None)
+    highest_file = max(mod.get_files().results, key=lambda file: parse(file.version or "0.0.0"), default=None)
     return highest_file.url if highest_file else ''
-
-
-def get_newest_mod_urls(mods):
-    return [get_highest_file_url(mod) for mod in mods]
 
 
 def download_file(url):
@@ -85,8 +57,10 @@ def download_file(url):
 
 
 def update_subbed_mods():
-    mods = get_subbed_mods()
-    urls_to_process = get_newest_mod_urls(mods)
+    config, client = get_or_create_client_config()
+    gameid = client.get_games(filters=modio.Filter({'name': config["game"]})).results[0].id
+    mods = client.get_my_subs(filters=modio.Filter({'game_id': gameid})).results
+    urls_to_process = [get_highest_file_url(mod) for mod in mods]
     for url in urls_to_process:
         download_file(url)
 
@@ -94,9 +68,13 @@ def update_subbed_mods():
 def extract_mods():
     for item in os.listdir(CACHE_PATH):
         if item.endswith(".zip"):
-            with zipfile.ZipFile(os.path.abspath(os.path.join(CACHE_PATH, item))) as zip_ref:
+            with zipfile.ZipFile(os.path.join(CACHE_PATH, item)) as zip_ref:
                 zip_ref.extractall(MODS_PATH)
 
 
-# update_subbed_mods()
-# extract_mods()
+def main():
+    update_subbed_mods()
+    extract_mods()
+
+
+main()
